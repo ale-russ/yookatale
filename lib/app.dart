9d56fe405c5b6adfa56415e0ac33192c3865cc03
ale-russ-kabbee <arussom@kabbee.org>
2023-08-08 12:14:40 +0300

    modifed folder structure

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,19 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'common/custom_appbar.dart';
-import 'notifiers/product_notifier.dart';
-import 'widgets/company_info_page.dart';
-import 'widgets/footer.dart';
-import 'widgets/motto.dart';
-import 'widgets/our_services.dart';
-import 'widgets/products.dart';
-import 'widgets/register_button.dart';
-import 'widgets/top_icons.dart';
+import 'features/common/custom_appbar.dart';
+import 'features/menu_notifier.dart';
+import 'features/notifiers/product_notifier.dart';
+import 'features/widgets/company_info_page.dart';
+import 'features/widgets/footer.dart';
+import 'features/widgets/menu_slider.dart';
+import 'features/widgets/motto.dart';
+import 'features/widgets/our_services.dart';
+import 'features/widgets/products.dart';
+import 'features/widgets/register_button.dart';
+import 'features/widgets/top_icons.dart';
 
 class App extends ConsumerStatefulWidget {
   const App({super.key});
@@ -21,6 +25,8 @@ class App extends ConsumerStatefulWidget {
 class _AppState extends ConsumerState<App> {
   final ScrollController scrollController = ScrollController();
   bool showFooter = false;
+  Offset? tapPosition;
+  bool keyboardIsVisible = false;
 
   @override
   void initState() {
@@ -47,51 +53,67 @@ class _AppState extends ConsumerState<App> {
     }
   }
 
+  @override
   Widget build(BuildContext context) {
     var productProvider = ref.watch(productsProvider);
-    var fruitProdcuts = ref.watch(fruiteProvider);
+    var fruitProducts = ref.watch(fruitProvider);
+    var isVisible = ref.watch(visibilityProvider);
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: CustomAppBar(context),
-      body: NotificationListener(
-        onNotification: (scrollNotification) {
-          if (scrollNotification is ScrollUpdateNotification) {
-            setState(() {
-              showFooter = scrollController.position.pixels >=
-                  scrollController.position.maxScrollExtent;
-            });
-          }
-          return showFooter;
-        },
-        child: SizedBox(
-          child: SingleChildScrollView(
-            controller: scrollController,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                const TopIcons(),
-                const Motto(),
-                const OurServices(),
-                ProductsPage(
-                  productProvider: productProvider,
-                  title: 'Popular Products',
+    return GestureDetector(
+      onTap: () {},
+      onTapDown: (details) {
+        tapPosition = details.globalPosition;
+        // Todo: Make the SliderMenu close when user tabs outside the slider
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Scaffold(
+              backgroundColor: Colors.white,
+              appBar: CustomAppBar(context),
+              body: NotificationListener(
+                onNotification: (scrollNotification) {
+                  if (scrollNotification is ScrollUpdateNotification) {
+                    setState(() {
+                      showFooter = scrollController.position.pixels >=
+                          scrollController.position.maxScrollExtent;
+                    });
+                  }
+                  return showFooter;
+                },
+                child: SizedBox(
+                  child: SingleChildScrollView(
+                    controller: scrollController,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        TopIcons(),
+                        const Motto(),
+                        const OurServices(),
+                        ProductsPage(
+                          productProvider: productProvider,
+                          title: 'Popular Products',
+                        ),
+                        const RegisterWidget(),
+                        ProductsPage(
+                          productProvider: fruitProducts,
+                          title: 'Fruits Products',
+                        ),
+                        const CompanyInfoPage(),
+                      ],
+                    ),
+                  ),
                 ),
-                const RegisterWidget(),
-                ProductsPage(
-                  productProvider: fruitProdcuts,
-                  title: 'Fruits Products',
-                ),
-                const CompanyInfoPage(),
-              ],
+              ),
+              bottomNavigationBar: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                height: showFooter ? 120.0 : 0.0,
+                child: const Footer(),
+              ),
             ),
-          ),
+            if (isVisible) SliderMenu(),
+          ],
         ),
-      ),
-      bottomNavigationBar: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        height: showFooter ? 90.0 : 0.0,
-        child: const Footer(),
       ),
     );
   }
